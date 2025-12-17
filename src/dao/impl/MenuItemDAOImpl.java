1fe1d0925f2c93890fd4050a52f7b89b3672fb0b
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-17 10:19:25 +0100

    implementation de la méthode getAllByCategory

---
+++ src/dao/impl/MenuItemDAOImpl.java
@@ -0,0 +1,52 @@
+package dao.impl;
+
+import dao.MenuItemDAO;
+import db.DatabaseConnection;
+import model.MenuItem;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuItemDAOImpl implements MenuItemDAO {
+	
+	@Override
+	public List<MenuItem> getAllByCategory(String category){
+		List<MenuItem> items = new ArrayList<>();
+		String sql = "SELECT * FROM menu_items WHERE category = ?";
+		
+		try( Connection connection = DatabaseConnection.getConnection();
+				PreparedStatement statement = connection.prepareStatement(sql)) {
+			
+			statement.setString(1,  category);
+			ResultSet rs = statement.executeQuery();
+			
+			while (rs.next()) {
+				MenuItem item = new MenuItem(
+						rs.getInt("id"),
+						rs.getString("name"),
+						rs.getString("category"),
+						rs.getDouble("price")
+						);
+				items.add(item);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return items;
+	}
+
+	@Override
+	public MenuItem getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MenuItem getByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
