c0a3788f73cc811c34f4c2df3432c4d541077454
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-17 11:01:55 +0100

    ajout de la classe Order dao qui implemente l'interface

---
+++ src/dao/impl/OrderDAOImpl.java
@@ -0,0 +1,27 @@
+package dao.impl;
+
+import dao.OrderDAO;
+import model.Order;
+
+
+public class OrderDAOImpl implements OrderDAO{
+
+	@Override
+	public int saveOrder(Order order) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLastOrderNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order getOrderById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
