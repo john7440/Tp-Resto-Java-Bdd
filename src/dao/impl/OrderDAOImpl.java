1308ef0c2176c0bf7271317c20bf3032fcaacfbd
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-17 13:31:51 +0100

    correction d'une typo au niveau de la requete avec COALESCE

--- src/dao/impl/OrderDAOImpl.java
+++ src/dao/impl/OrderDAOImpl.java
@@ -89,7 +89,7 @@ public class OrderDAOImpl implements OrderDAO{
 	@Override
 	public int getLastOrderNumber() {
 		// COALESCE retourne le 1er qui n'est pas NULL
-		String sql = "SELECT COALESCE(MAX(order_number,0) as last_order FROM orders";
+		String sql = "SELECT COALESCE(MAX(order_number),0) as last_order FROM orders";
 		
 		try (Connection connection = DatabaseConnection.getConnection();
 			 PreparedStatement statement = connection.prepareStatement(sql);
