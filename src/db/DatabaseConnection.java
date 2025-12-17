aca4c867738ad83ad42a348289e500eb43ab71be
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-17 13:30:56 +0100

    modification avec infos en dur pour corriger bug d'accessionn a la bdd

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -3,26 +3,22 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class DatabaseConnection {
 	private static Connection connection;
 	
+	private static final String URL = "jdbc:mariadb://localhost:3308/restaurant_db";
+	private static final String USER = "root";
+	private static final String PASSWORD = "Bi3rs077ZBGSv4lM98w8";
+	
+	
 	public static Connection getConnection() throws SQLException{
 		if (connection == null || connection.isClosed()) {
 			try {
-				Properties props = new Properties();
-				props.load(new FileInputStream("src/db/db.properties"));
-				
-				String url = props.getProperty("db.url");
-                String user = props.getProperty("db.user");
-                String password = props.getProperty("db.password");
-				
-                connection = DriverManager.getConnection(url, user, password);
-			} catch (IOException e) {
-				throw new SQLException("Erreur de configuration: ", e);
+				Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			} catch (ClassNotFoundException e) {
+				throw new SQLException("Maria DB non trouvé", e);
 			}
 		}
 		
